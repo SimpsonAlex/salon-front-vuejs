7b945887c58b29d6d39e7b0a508246e5f7896bdf
Alex <simpson_alex@ukr.net>
2019-10-17 17:24:11 +0300

    initial

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
+var history = require('connect-history-api-fallback');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var serveStatic = require('serve-static')
 
 express()
-  .use(express.static(path.join(__dirname, 'dist')))
+  .use(serveStatic(path.join(__dirname, 'dist'))).use(history())
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
