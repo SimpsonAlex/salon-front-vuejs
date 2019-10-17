53fcba30d637464bc6303e1a3efb2eb7755809f5
Alex <simpson_alex@ukr.net>
2019-10-17 17:58:15 +0300

    initial

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+//
+// var history = require('connect-history-api-fallback');
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+// var serveStatic = require('serve-static')
+//
+// express()
+//   .use(serveStatic(path.join(__dirname, 'dist'))).use(history())
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-var history = require('connect-history-api-fallback');
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-var serveStatic = require('serve-static')
+const express = require('express');
+const port = process.env.PORT || 5000;
+const app = express()
 
-express()
-  .use(serveStatic(path.join(__dirname, 'dist'))).use(history())
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(__dirname + "/dist"));
+app.get(/.*/, function(req, res){
+    res.sendfile(__dirname + '/dist/index.html')
+});
+app.listen(port);
