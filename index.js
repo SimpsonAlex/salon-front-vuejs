eb8c56986d031ace1cf10766393f5324efe9ca32
Alex <simpson_alex@ukr.net>
2019-10-17 14:59:36 +0300

    initial

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+var app = express()
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
